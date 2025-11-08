37edb0b7cf86df2a5007ee65ada57ecf321ee12f
eodudrepublic <eodudrepublic@gmail.com>
2025-11-08 17:21:24 +0900

    chore: App.vue 수정 - 작업을 위한 파일 추가 + .gitignore로 등록

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>Hello World!</h1>
+  <h3>I am eodudrepublic!</h3>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Vue 공부하려고 만든 거긴 한데 이걸 해커톤 끝나고 다시 들어올지는 모르겠어요:
+    <a href="https://github.com/eodudrepublic/Vue_tutorial" target="_blank" rel="noopener"
+      >프로젝트 레포지토리</a
+    >
   </p>
 </template>
 
